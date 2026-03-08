b220543d8df84a344a23e38c265ac189cba3be43
Brian Dang <pandabrian1@gmail.com>
2026-03-08 01:37:34 -0700

    Deployed Website

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/briandangdev-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
